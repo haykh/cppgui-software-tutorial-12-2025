5feab9a407bd664f2ad060cc5ebd24efc005effd
haykh <haykh.astro@gmail.com>
2025-12-09 12:06:45 -0500

    nix lib paths upd

--- shell.nix
+++ shell.nix
@@ -5,37 +5,48 @@
 
 let
   name = "imgui-demo";
+  xlibs = with pkgs; [
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXinerama
+    libGL
+    glfw
+  ];
 in
 pkgs.mkShell {
 
   name = "${name}-env";
-  nativeBuildInputs = with pkgs; [
-    zlib
-    cmake
-    pkgs."python${py}"
-    pkgs."python${py}Packages".pip
+  nativeBuildInputs =
+    with pkgs;
+    [
+      zlib
+      cmake
+      pkgs."python${py}"
+      pkgs."python${py}Packages".pip
 
-    libgcc
-    clang-tools
-    glfw
+      libgcc
+      clang-tools
 
-    cmake-format
-    cmake-lint
-    neocmakelsp
-    taplo
-    vscode-langservers-extracted
-    black
-    pyright
+      cmake-format
+      cmake-lint
+      neocmakelsp
+      taplo
+      vscode-langservers-extracted
+      black
+      pyright
 
-    nodePackages.nodejs
-    emmet-ls
-    typescript-language-server
-    taplo
-    yaml-language-server
-    prettierd
-    eslint
-    mdformat
-  ];
+      nodePackages.nodejs
+      emmet-ls
+      typescript-language-server
+      taplo
+      yaml-language-server
+      prettierd
+      eslint
+      mdformat
+    ]
+    ++ xlibs;
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([
     pkgs.stdenv.cc.cc
@@ -43,8 +54,7 @@ pkgs.mkShell {
   ]);
 
   shellHook = ''
-    BLUE='\033[0;34m'
-    NC='\033[0m'
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath xlibs}:$LD_LIBRARY_PATH
 
     echo -e "${name} nix-shell activated"
   '';
